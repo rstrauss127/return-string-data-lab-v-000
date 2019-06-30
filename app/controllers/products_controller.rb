873e5fda239892be4949dde07249f448f60d258f
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 17:58:46 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,10 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render plain: @product.description
   end
+
+  def inventory
+    @product = Product.find(params[:id])
+  end
 private
     def product_params
       params.require(:product).permit(:name, :description, :inventory, :price)
