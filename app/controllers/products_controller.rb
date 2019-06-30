51cc9a49bca3bfdf292c7327c70ef3499556bf89
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 17:51:37 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
+  end
 end
